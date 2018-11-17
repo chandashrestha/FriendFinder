1c8cd49578329b5dee253841e063d403c71a2ed4
Chanda Shrestha <chandashrestha89@gmail.com>
2018-11-16 23:59:52 -0600

    routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,32 +7,32 @@ var friendsList = [
   {
     name: "Pawan",
     photo: "https://picsum.photos/200/300/?random",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    scores: [1, 5, 1, 4, 4, 5, 1, 2, 5, 4]
   },
   {
     name: "Shrawan",
     photo: "https://picsum.photos/200/300/?random",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    scores: [4, 1, 5, 1, 4, 4, 5, 1, 2, 5]
   },
   {
     name: "Om",
     photo: "https://picsum.photos/200/300/?random",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    scores: [5, 4, 1, 5, 1, 4, 4, 5, 1, 2]
   },
   {
     name: "Nebhas",
     photo: "https://picsum.photos/200/300/?random",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    scores: [1, 2, 5, 4, 1, 5, 1, 4, 4, 5]
   },
   {
     name: "Gerrardo",
     photo: "https://picsum.photos/200/300/?random",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    scores: [, 1, 2, 5, 4, 1, 4, 5, 1, 4]
   },
   {
     name: "Michael",
     photo: "https://picsum.photos/200/300/?random",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    scores: [5, 1, 1, 2, 5, 4, 1, 5, 4, 1]
   },
   {
     name: "Kiresh",
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,14 +3,19 @@
 
 var path = require("path");
 
-module.exports = function(app){
-
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
-
-// Basic route that sends the user first to the AJAX Page
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "survey.html"));
-});
-}
+module.exports = function(app) {
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  // Basic route that sends the user first to the AJAX Page
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+  });
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+  app.post("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+};
+var bestmatches;
