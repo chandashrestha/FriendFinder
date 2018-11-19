2b6457c9391cd8c1935cfa5e4fec24c634580091
Chanda Shrestha <chandashrestha89@gmail.com>
2018-11-19 15:38:14 -0600

    SURVEY

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,13 +4,13 @@
 var path = require("path");
 
 module.exports = function(app) {
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-  // Basic route that sends the user first to the AJAX Page
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-  });
+  // app.get("/", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/home.html"));
+  // });
+  // // Basic route that sends the user first to the AJAX Page
+  // app.get("/survey", function(req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/survey.html"));
+  // });
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
