d0572ee231944851b155bc4f2f9a636b6b89136e
Chanda Shrestha <chandashrestha89@gmail.com>
2018-11-19 15:04:42 -0600

    apiroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,36 +5,32 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
   // Basic route that sends the user first to the AJAX Page
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
   app.post("/api/friends", function(req, res) {
-    
     var counterScore = 50;
     var newFriendScore = req.body.scores;
     var closestFriend;
 
-
     for (var i = 0; i < path.length; i++) {
+      var currentFriendScore = path[i].scores;
+      var totalScore = 0;
 
-        var currentFriendScore = path[i].scores;
-        var totalScore = 0;
+      for (var j = 0; j < currentFriendScore.length; j++) {
+        totalScore += Math.abs(currentFriendScore[j] - newFriendScore[j]);
+      }
 
-
-        for (var j = 0; j < currentFriendScore.length; j++) {
-            totalScore += Math.abs(currentFriendScore[j] - newFriendScore[j]);
-        }
-
-        if (totalScore <= counterScore) {
-            counterScore = totalScore;
-            closestFriend = path[i];
-        }
+      if (totalScore <= counterScore) {
+        counterScore = totalScore;
+        closestFriend = path[i];
+      }
     }
     path.push(req.body);
     res.json(friends);
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,11 +5,11 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   // Basic route that sends the user first to the AJAX Page
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 };
