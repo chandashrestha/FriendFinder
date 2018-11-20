039c57e95389a65ecf63f49ac910fc28a23fb1f4
Chanda Shrestha <chandashrestha89@gmail.com>
2018-11-20 11:46:54 -0600

    apiroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,26 +1,18 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-
-var path = require("path");
+var friendsInfo = require("../data/friends");
 
 module.exports = function(app) {
-  // app.get("/", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/home.html"));
-  // });
-  // // Basic route that sends the user first to the AJAX Page
-  // app.get("/survey", function(req, res) {
-  //   res.sendFile(path.join(__dirname, "../public/survey.html"));
-  // });
   app.get("/api/friends", function(req, res) {
-    res.json(friends);
+    res.json(friendsInfo);
   });
   app.post("/api/friends", function(req, res) {
     var counterScore = 50;
     var newFriendScore = req.body.scores;
     var closestFriend;
 
-    for (var i = 0; i < path.length; i++) {
-      var currentFriendScore = path[i].scores;
+    for (var i = 0; i < friendsInfo.length; i++) {
+      var currentFriendScore = friendsInfo[i].scores;
       var totalScore = 0;
 
       for (var j = 0; j < currentFriendScore.length; j++) {
@@ -29,11 +21,11 @@ module.exports = function(app) {
 
       if (totalScore <= counterScore) {
         counterScore = totalScore;
-        closestFriend = path[i];
+        closestFriend = friendsInfo[i];
       }
     }
-    path.push(req.body);
-    res.json(friends);
+    friendsInfo.push(req.body);
+    res.json(closestFriend);
   });
 };
-var bestmatches;
+
